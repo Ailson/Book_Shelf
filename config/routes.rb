e08f275bfed7763fea79c2fce21487eab4068ebd
Leonardo Costa <leotasco@gmail.com>
2012-08-14 22:44:40 -0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -55,9 +55,6 @@ BookShelf::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
