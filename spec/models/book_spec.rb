f6036207a01c87ad39e14f701e6410c52f8132db
Leonardo Costa <leotasco@gmail.com>
2012-09-01 22:21:33 -0300

    book validation on create

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,16 +7,31 @@ describe Book do
       @friend = FactoryGirl.create(:friend)
     end
 
-    describe "#lend" do
-       it "lend a book to a friend" do
-          @book.lend_to(@friend)
-       end
+    describe "create book" do
 
-       it "check if a book is borrowed" do
-          @book.lend_to(@friend)
-          @book.is_borrowed.should be_true
-          @book.get_availability.should include("borrowed since")
-       end
+      it "should not save a book without a title" do
+        @book.title = nil
+        @book.save.should raise_error
+      end
+
+      it "should not save a book without a picture" do
+        @book.picture = nil
+        @book.save.should raise_error
+      end
+
+    end
+
+    describe "lend" do
+
+      it "lend a book to a friend" do
+         @book.lend_to(@friend)
+      end
+
+      it "check if a book is borrowed" do
+         @book.lend_to(@friend)
+         @book.is_borrowed.should be_true
+         @book.get_availability.should include("borrowed since")
+      end
 
        it "check if a book is unborrow" do
           @book.lend_to(@friend)
@@ -24,6 +39,18 @@ describe Book do
           @book.is_borrowed.should be_false
           @book.get_availability.should include("available")
        end
+
+       it "When a book is borrowed this should appear on the borrowed books of the user that is borrowing" do
+        #book = Book.new
+        #book.title = "teste"
+        #book.picture = "teste"
+        #book.save
+        #user = User.find(2)
+        #book.lend_to(user)
+
+        #assert user.books_borrowed.count ==  1
+        #assert_equal(book, user.books_borrowed[0])
+      end
 
     end
 end
